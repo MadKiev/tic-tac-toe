97fc00870dd447f5f2ee25b943aab8672af010ed
Shevchenko3141 <MadKiev@gmail.com>
2018-03-13 17:44:07 +0200

    bugs fixed

--- src/js.js
+++ src/js.js
@@ -1,5 +1,4 @@
-window.onload = function() {
-
+window.onload = function () {
 
     for (var i = 0; i < 9; i++) {
         document.getElementById('game').innerHTML += '<div class="block"></div>';
@@ -8,70 +7,52 @@ window.onload = function() {
 
     var count = 0;
 
-
     document.getElementById('game').onclick = function (e) {
 
         console.log(e);
         var symbol = '0';
         if (e.target.className !== 'block') {
         } else {
-            if (count % 2 === 0)
-                symbol = '0';
-             // else
-             //     symbol = 'x';
-            makeRandomStep();
-            checkWin();
-            checkDraw();
             e.target.innerHTML = symbol;
             e.target.className = 'block';
-            // count++;
+            makeRandomStep();
+            checkWin('0');
+            checkWin('x');
             if (count > 4)
-                checkWin(symbol);
-            return false;
-
-
+                checkWin();
         }
-    }
-
+    };
     function checkWin(s) {
-
-        checkDraw(s);
-
         console.log(s);
         var allblock = document.getElementsByClassName('block');
         console.log(allblock);
         for (var i = 0; i < 9; i = i + 3) {
-            if (allblock[i].innerHTML === s && allblock[i + 1].innerHTML === s && allblock[i + 2].innerHTML === s)
+            if (allblock[i].innerHTML === s && allblock[i + 1].innerHTML === s && allblock[i + 2].innerHTML === s) {
                 alert('Winner "' + s + '"');
-            return true;
+            }
         }
         for (i = 0; i < 3; i++) {
-            if (allblock[i].innerHTML === s && allblock[i + 3].innerHTML === s && allblock[i + 6].innerHTML === s)
+            if (allblock[i].innerHTML === s && allblock[i + 3].innerHTML === s && allblock[i + 6].innerHTML === s) {
                 alert('Winner "' + s + '"');
-            return true;
+            }
         }
         //diagonal
-        if (allblock[0].innerHTML === s && allblock[4].innerHTML === s && allblock[8].innerHTML === s)
+        if (allblock[0].innerHTML === s && allblock[4].innerHTML === s && allblock[8].innerHTML === s) {
             alert('Winner "' + s + '"');
-        return true;
+        }
         //reverse diagonal
-        if (allblock[2].innerHTML === s && allblock[4].innerHTML === s && allblock[6].innerHTML === s)
+        if (allblock[2].innerHTML === s && allblock[4].innerHTML === s && allblock[6].innerHTML === s) {
             alert('Winner "' + s + '"');
-        //return true;
+        }
 
-    }
-
-
-
-    function checkDraw() {
-        if (count == 9)
+        if (count === 9) {
             alert('draw!');
-        return false;
+        }
 
     }
+
 
     function makeRandomStep() {
-
         var cells = document.getElementById('game').getElementsByClassName('block');
         var emptyCells = [];
         for (var i = 0; i < cells.length; i++) {
@@ -81,22 +62,9 @@ window.onload = function() {
             }
 
         }
-        random = Math.floor(Math.random() * (emptyCells.length));
+        var random = Math.floor(Math.random() * (emptyCells.length));
         cells[emptyCells[random]].innerHTML = "x";
     }
-}
-/*
-function move(id, role) {
-    if(t[id]) return false;
-    t[id]=role;
-    document.getElementById(id).className = 'cell ' + role;
-    !checkWin() ? (role=='player') ? ai() : null :reset();
-    }
+};
 
 
-function ai() {
-    //0
-    var id = Math.floor(Math.random()*9);
-    t[id] ? ai():move(id,'ai');
-}
- */
